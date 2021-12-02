80941f1a11614fc35da90fba193e87cc7ea15389
Luis Del Orbe Martinez <luismigueldelorbe@hotmail.com>
2021-12-02 15:26:48 -0400

    Fixing

--- routes/empresa/empresa.route.ts
+++ routes/empresa/empresa.route.ts
@@ -2,11 +2,12 @@ import {Router} from 'express';
 import { deleteEmpresa, getEmpresa , postEmpresa, updateEmpresa} from '../../Controllers/empresas.controller';
 
 const router  = Router();
-const endPointEmpresas = '/api/empresa'
+const endPointEmpresas = '/api/empresas';
+const empresa = 'empresa';
 
-router.get(`${endPointEmpresas}/:id?`,getEmpresa); 
+router.get(`${endPointEmpresas}/${empresa}/:id?`,getEmpresa); 
 router.post(`${endPointEmpresas}`,postEmpresa);
-router.put(`${endPointEmpresas}/deleteEmpresa/:id`,deleteEmpresa);
-router.put(`${endPointEmpresas}/updateEmpresa/:id`,updateEmpresa)
+router.put(`${endPointEmpresas}/${empresa}/:id`,updateEmpresa);
+router.delete(`${endPointEmpresas}/${empresa}/:id`,deleteEmpresa)
 
 export default router;
