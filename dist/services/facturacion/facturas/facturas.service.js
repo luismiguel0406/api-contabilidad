b0a792a9db5d2d6e64d5d6aaf869c00377f986da
Luis Del Orbe Martinez <luismigueldelorbe@hotmail.com>
2022-01-02 00:56:47 -0400

    facturas

--- dist/services/facturacion/facturas/facturas.service.js
+++ dist/services/facturacion/facturas/facturas.service.js
@@ -1,6 +1,53 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const detalleFactura_1 = __importDefault(require("../../../models/Facturacion/facturas/detalleFactura"));
+const factura_model_1 = __importDefault(require("../../../models/Facturacion/facturas/factura.model"));
+const detalleImpuestos_1 = __importDefault(require("../../../models/Facturacion/impuestos/detalleImpuestos"));
 class FacturasService {
+    addFactura(body) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const factura = yield factura_model_1.default.create(body);
+                return factura;
+            }
+            catch (error) {
+                return error;
+            }
+        });
+    }
+    deleteFactura(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield factura_model_1.default.update({ estado: "0" }, { where: { id } });
+        });
+    }
+    getFacturas(id = null) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const facturasResult = id === null
+                ? yield factura_model_1.default.findAll({
+                    include: [
+                        {
+                            model: detalleFactura_1.default,
+                            required: true,
+                            include: [{ model: detalleImpuestos_1.default, required: true }],
+                        },
+                    ],
+                })
+                : yield factura_model_1.default.findAll();
+            return facturasResult;
+        });
+    }
 }
 exports.default = FacturasService;
 //# sourceMappingURL=facturas.service.js.map
--- services/facturacion/facturas/facturas.service.ts
+++ services/facturacion/facturas/facturas.service.ts
@@ -1,7 +1,36 @@
+import { IFactura } from "../../../interfaces/factura.interface";
+import detallesFactura from "../../../models/Facturacion/facturas/detalleFactura";
+import facturas from "../../../models/Facturacion/facturas/factura.model";
+import detallesImpuesto from "../../../models/Facturacion/impuestos/detalleImpuestos";
 
 export default class FacturasService {
+  async addFactura(body: IFactura) {
+    try {
+      const factura = await facturas.create(body);
+      return factura;
+    } catch (error) {
+      return error;
+    }
+  }
 
+  async deleteFactura(id: string) {
+    await facturas.update({ estado: "0" }, { where: { id } });
+  }
 
-
-
+  async getFacturas(id: any = null) {
+    const facturasResult =
+      id === null
+        ? await facturas.findAll({
+            include: [
+              {
+                model: detallesFactura,
+                required: true,
+                include: [{ model: detallesImpuesto, required: true }],
+              },
+            ],
+          })
+        : await facturas.findAll();
+        
+    return facturasResult;
+  }
 }
