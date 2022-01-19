08ca4bf65a782fbfa98e0bf7ee22bf43b61a9ae0
Luis Del Orbe Martinez <luismigueldelorbe@hotmail.com>
2022-01-19 00:09:46 -0400

    perfil

---
+++ dist/models/Perfiles/perfil.model.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
+const connectionDB_1 = __importDefault(require("../../Database/connectionDB"));
+const perfil = connectionDB_1.default.define("perfil", {
+    descripcion: {
+        type: sequelize_1.DataTypes.STRING(50),
+        allowNull: false
+    },
+    estado: {
+        type: sequelize_1.DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+    createdAt: {
+        type: sequelize_1.DataTypes.DATE,
+        allowNull: false,
+    },
+    updatedAt: {
+        type: sequelize_1.DataTypes.DATE,
+    },
+});
+//# sourceMappingURL=perfil.model.js.map
---
+++ src/models/Perfiles/perfil.model.ts
@@ -0,0 +1,24 @@
+
+import { DataTypes } from "sequelize";
+import conexion from "../../Database/connectionDB";
+
+const perfil = conexion.define( "perfil",
+{
+    descripcion:{
+        type: DataTypes.STRING(50),
+        allowNull:false
+    },
+    estado:{
+        type:DataTypes.BOOLEAN,
+        allowNull:false,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+      },
+
+})
+  
