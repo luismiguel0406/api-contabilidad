839c2f2e686578c4720bddace854835055fa7c5a
Luis Del Orbe Martinez <luismigueldelorbe@hotmail.com>
2023-11-07 23:40:47 -0400

    modelos de tipos en proveedores

---
+++ src/models/Proveedores/tipoServicio.mode.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Model } from "sequelize";
+import conexion from "../../Database";
+import { TTipoGenerico } from "types";
+
+const tipoServicio = conexion.define<Model<TTipoGenerico>>(
+  "tipoServicio",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    descripcion: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    },
+    estado: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    usuario: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    terminal: {
+      type: DataTypes.STRING,
+    },
+  },
+  { schema: "PROVEEDORES" }
+);
+
+export default tipoServicio;
